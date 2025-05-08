99d17c110d58d58a7e6e9f7fe7b98ac52a356fd6
Jamie Parkinson <jamie.parkinson@prospect.org.uk>
2025-05-08 14:53:11 +0100

    Switch to getting the key from the cache

--- get-outlook-365-directory.js
+++ get-outlook-365-directory.js
@@ -1,33 +1,64 @@
-async function getGraphAccessToken() {
-  const exchangePostData = {
-    __type: "TokenRequest:#Exchange",
-    Resource: "https://graph.microsoft.com/",
-  };
+function base64DecToArr(base64String) {
+  let encodedString = base64String.replace(/-/g, "+").replace(/_/g, "/");
+  switch (encodedString.length % 4) {
+    case 0:
+      break;
+    case 2:
+      encodedString += "==";
+      break;
+    case 3:
+      encodedString += "=";
+      break;
+    default:
+      throw createBrowserAuthError(BrowserAuthErrorCodes.invalidBase64String);
+  }
+  const binString = atob(encodedString);
+  return Uint8Array.from(binString, (m) => m.codePointAt(0) || 0);
+}
 
-  console.log("Requesting an access token for Graph...");
-  const tokenRequest = await fetch(
-    "https://outlook.office.com/owa/service.svc?action=GetAccessTokenforResource",
+async function getGraphAccessToken() {
+  // It is easier to get the token from the MSAL cache than it is to ask for a new one
+  const { value } = await cookieStore.get("msal.cache.encryption");
+  const encryption = JSON.parse(decodeURIComponent(value));
+
+  const msalKey = Object.keys(localStorage).find((k) =>
+    k.startsWith("msal.token.keys")
+  );
+  const msalTokenKeys = JSON.parse(localStorage.getItem(msalKey));
+  const accessTokenKey = msalTokenKeys.accessToken.find((k) =>
+    k.includes("https://graph.microsoft.com/user.readbasic.all")
+  );
+  const encryptedToken = JSON.parse(localStorage.getItem(accessTokenKey));
+
+  const data = base64DecToArr(encryptedToken.data);
+  const nonce = base64DecToArr(encryptedToken.nonce);
+  const clientId = msalKey.split(".")[3];
+  const baseKey = await window.crypto.subtle.importKey(
+    "raw",
+    base64DecToArr(encryption.key),
+    "HKDF",
+    false,
+    ["deriveKey"]
+  );
+  const derivedKey = await window.crypto.subtle.deriveKey(
     {
-      headers: {
-        action: "GetAccessTokenforResource",
-        "content-type": "application/json; charset=utf-8",
-        "x-owa-urlpostdata": encodeURIComponent(
-          JSON.stringify(exchangePostData)
-        ),
-        "x-req-source": "Mail",
-      },
-      method: "POST",
-      mode: "cors",
-      credentials: "include",
-    }
+      name: "HKDF",
+      salt: nonce,
+      hash: "SHA-256",
+      info: new TextEncoder().encode(clientId),
+    },
+    baseKey,
+    { name: "AES-GCM", length: 256 },
+    false,
+    ["encrypt", "decrypt"]
   );
-  const { AccessToken, AccessTokenExpiry } = await tokenRequest.json();
-  console.log(
-    `%cSuccessfully retrieved token %c(expires: ${AccessTokenExpiry})`,
-    "color: green",
-    "color: gray; font-style: italic"
+  const decrypted = await window.crypto.subtle.decrypt(
+    { name: "AES-GCM", iv: new Uint8Array(12) },
+    derivedKey,
+    data
   );
-  return AccessToken;
+
+  return JSON.parse(new TextDecoder().decode(decrypted)).secret;
 }
 
 async function* scrollDirectory(graphToken) {
